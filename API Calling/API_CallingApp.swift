56d7223db946dbb8bd0e63ad1cc5db3b4f8dcd14
Faiz Ali <fali@bsd220.org>
2023-02-26 13:41:06 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Faiz Ali on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
